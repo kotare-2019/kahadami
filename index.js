36308dcc886beca55ac75ea8fdc1f1112b13f850
Danielrhodesy <37157488+Danielrhodesy@users.noreply.github.com>
2019-06-06 11:53:05 +1200

    test success lol

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server')
-// const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 server.listen(port, function () {
@@ -8,7 +8,7 @@ server.listen(port, function () {
 })
 
 
- var port = process.env.PORT || 3000
+//  var port = process.env.PORT || 3000
 
 // ─student@mac-013 ~/workspace/pupparazzi  ‹pano-ben*› 
 // ╰─$ lsof -i 3000
