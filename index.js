16001cc4c1fed9281a5dca399f34cab05738d5a2
Danielrhodesy <37157488+Danielrhodesy@users.noreply.github.com>
2019-06-06 11:38:44 +1200

    change port

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const server = require('./server')
+const port = process.env.PORT || 3000
 
 
 server.listen(port, function () {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port', port)
 })
-var port = process.env.PORT || 3000
 
 
 // ─student@mac-013 ~/workspace/pupparazzi  ‹pano-ben*› 
