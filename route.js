e6098adbf712e1e0a0e9354fd8adaa3664dec3aa
Hanh <47132194+hanh008@users.noreply.github.com>
2019-06-06 16:46:58 +1200

    change puppy to trees

--- route.js
+++ route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const puppiesRoutes = express.Router()
+const treeRoutes = express.Router()
 const data = require('./data')
 const fs = require('fs')
 
@@ -10,32 +10,32 @@ const viewData = {
 
 
 
-puppiesRoutes.get('/', (req, res) => {
-    res.render('./puppies/index', data);  
+treeRoutes.get('/', (req, res) => {
+    res.render('./partials/index', data);  
 })
 
-puppiesRoutes.get('/:id', (req, res) => {
-    let puppyProfile = data.puppies.find(item =>{
+treeRoutes.get('/:id', (req, res) => {
+    let treeProfile = data.trees.find(item =>{
         return req.params.id == item.id
     })
-    res.render('./puppies/view', puppyProfile)
+    res.render('./partials/view', treeProfile)
 })
 
-puppiesRoutes.get('/edit/:id', (req, res) => {
-    let puppyProfile = data.puppies.find(item =>{
+treeRoutes.get('/edit/:id', (req, res) => {
+    let treeProfile = data.trees.find(item =>{
         return req.params.id == item.id
     })
-    res.render('./puppies/edit', puppyProfile) 
+    res.render('./partials/edit', treeProfile) 
 })
 
 
 
-puppiesRoutes.post('/edit/:id', (req, res)=>{
+treeRoutes.post('/edit/:id', (req, res)=>{
     
     req.body.id = req.params.id
-    for (var i = 0; i< data.puppies.length; i++){
-        if(data.puppies[i].id == req.params.id){
-            data.puppies[i] = req.body 
+    for (var i = 0; i< data.trees.length; i++){
+        if(data.trees[i].id == req.params.id){
+            data.trees[i] = req.body 
         }
     }
     fs.writeFile('data.json', JSON.stringify(data, null, 2), 'utf8', (err)=>{ 
@@ -48,6 +48,6 @@ puppiesRoutes.post('/edit/:id', (req, res)=>{
 
 
 module.exports = {
-    puppiesRoutes
+    treeRoutes
 }
 
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-const { puppiesRoutes } = require('./route')
+const {
+  treeRoutes
+} = require('./route')
 
 
 const server = express()
-server.use(express.urlencoded({extended: true}))
+server.use(express.urlencoded({
+  extended: true
+}))
+
+
+
 // Middleware
 
-
-
 server.get('/', (req, res) => {
-  res.send("<a href='/puppies'> <img style='height: 100%; width:100%;' src='https://img.buzzfeed.com/buzzfeed-static/static/2016-10/6/16/campaign_images/buzzfeed-prod-fastlane01/the-36-funniest-dog-pics-known-to-man-2-23061-1475785734-1_dblbig.jpg'></a>")
+  res.redirect('/home')
 })
 
-server.use('/puppies', puppiesRoutes)
+server.use('/home', treeRoutes)
 
 
 server.engine('hbs', hbs({
@@ -22,6 +27,8 @@ server.engine('hbs', hbs({
 }))
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
-server.use(express.urlencoded({extended: false}))
+server.use(express.urlencoded({
+  extended: false
+}))
 
 module.exports = server
